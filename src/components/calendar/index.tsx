17a29262acee964efa9e80baf5abd182f84cb4ee
chakriyaP <P.chakriya@kkumail.com>
2025-03-12 23:01:46 +0700

    test git accout

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -4,10 +4,7 @@ const Calendar = () => {
   return (
     <div className="p-4 border rounded-md shadow-md bg-white">
       <p className="text-sm text-gray-500">(Calendar UI placeholder)</p>
-      <button
-        className="mt-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600"
-        // onClick={() => onSelect(new Date())}
-      >
+      <button className="mt-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600">
         Pick Today
       </button>
     </div>
